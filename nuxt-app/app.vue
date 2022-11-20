aeaefb51a485663ca99f1421af05b1581dc9ac60
Chris Castle <chris.castle@getyourguide.com>
2022-11-20 09:52:09 +0100

    x

---
+++ nuxt-app/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>{{counter.count}}</h1>
+    <v-btn @click="counter.increment">Increment</v-btn>
+  </div>
+</template>
+
+<script setup>
+import { useCounterStore } from "@/stores/store";
+const counter = useCounterStore();
+</script>
